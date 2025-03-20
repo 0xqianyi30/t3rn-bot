088beae7a62a61921bdfb102038fd35738784ef4
0xqianyi30 <5e13i77e@gmaiilf.com>
2025-03-21 00:36:52 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -7,7 +7,7 @@ networks = {
         'contract_address': '0x22B65d0B9b59af4D3Ed59F18b9Ad53f5F4908B54' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'rpc_url': 'https://sepolia.optimism.io',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
